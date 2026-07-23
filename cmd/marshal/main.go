baf0fe80a5355ca1ec57d39536a12cf9dda54ec0
Alec Pullen <alecpullen24@gmail.com>
2026-07-23 16:55:10 +1000

    sdd2/T16: add marshal history CLI subcommand

--- cmd/marshal/main.go
+++ cmd/marshal/main.go
@@ -12,6 +12,7 @@ import (
 
 var appRunner = app.Run
 var acpRunner = acp.Run
+var historyRunner = runHistory
 
 func main() {
 	if err := run(context.Background(), os.Args[1:], os.Stdin, os.Stdout, os.Stderr); err != nil {
@@ -21,6 +22,9 @@ func main() {
 }
 
 func run(ctx context.Context, args []string, stdin io.Reader, stdout, stderr io.Writer) error {
+	if len(args) > 0 && args[0] == "history" {
+		return historyRunner(ctx, args[1:], stdout)
+	}
 	if len(args) > 0 && args[0] == "acp" {
 		return acpRunner(ctx, stdin, stdout, stderr)
 	}
